4be0987de1c551547a9e7a2357e9b7ee99286894
Edd224 <84470779+Edd224@users.noreply.github.com>
2021-07-06 11:35:45 +0200

    Btn X

---
+++ js/dist/main.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Select DOM items
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
+var menuNav = document.querySelector('.menu-nav');
+var menuBranding = document.querySelector('.menu-branding');
+var navItems = document.querySelectorAll('.nav-item'); // Set Initial State Of Menu
+
+var showMenu = false;
+menuBtn.addEventListener('click', togglenMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(function (item) {
+      return item.classList.add('show');
+    });
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(function (item) {
+      return item.classList.remove('show');
+    }); // Set Menu State
+
+    showMenu = false;
+  }
+}

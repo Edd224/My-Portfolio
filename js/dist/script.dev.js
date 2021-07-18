7888fe52cb7c298e2d184b97b3310b5a88ea36b5
Edd224 <84470779+Edd224@users.noreply.github.com>
2021-07-18 14:52:32 +0200

    js. background anime

---
+++ js/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(function () {
+  $("p").hide();
+  $("p").slideDown(3000);
+  $("h3").animate({
+    "font-size": "30px"
+  }, 1500);
+});
---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function()
+{
+
+ 
+  $("p").hide();
+  $("p").slideDown(3000);
+
+
+
+  $("h3").animate({
+    "font-size":"30px"
+    
+
+  },1500);
+    
+
+
+    
+
+});
